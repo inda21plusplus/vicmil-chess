d88a04d81ba70b432f0f58d5cb636f21c174467a
vnomarrow <vnomarrow@gmail.com>
2021-10-03 19:04:49 +0200

    add chess UI

--- chess_gui_with_networking/src/main.rs
+++ chess_gui_with_networking/src/main.rs
@@ -2,6 +2,7 @@ use ggez::{Context, ContextBuilder, GameResult};
 use ggez::graphics::{self, Color, Rect};
 use ggez::event::{self, EventHandler, MouseButton};
 use glam::*;
+use chess_engine::chess_game::*;
 
 const SCREEN_WIDTH: f32 = 800.0;
 const SCREEN_HEIGHT: f32 = 800.0;
@@ -18,7 +19,8 @@ fn main() {
     // Create an instance of your event handler.
     // Usually, you should provide it with the Context object to
     // use when setting your game up.
-    let my_game = MyGame::new(&mut ctx).unwrap();
+    let mut my_game = MyGame::new(&mut ctx).unwrap();
+    my_game.game.set_up_board();
 
     // Run!
     event::run(ctx, event_loop, my_game);
@@ -40,7 +42,7 @@ struct MyGame {
     black_square: graphics::Image,
     white_square: graphics::Image,
     mouse_button_press_down: Option<ggez::mint::Point2<f32>>,
-    //game: chess_logic::GAME
+    game: chess_engine::chess_game::Game
 }
 
 pub fn get_square_from_mouse_pos(pos: ggez::mint::Point2<f32>) -> Result<ggez::mint::Point2<u8>, String> {
@@ -71,7 +73,7 @@ impl MyGame {
         let black_square = graphics::Image::new(ctx, "/black_square.png")?;
         let white_square = graphics::Image::new(ctx, "/white_square.png")?;
 
-        //let game = chess_logic::init_game();
+        let game = chess_engine::chess_game::Game::new();
 
         let s = MyGame {
             black_rook,
@@ -89,49 +91,34 @@ impl MyGame {
             black_square,
             white_square,
             mouse_button_press_down: None,
-            //game,
+            game,
         };
 
         Ok(s)
     }
 
-    /*pub fn get_board_piece_image(&mut self, piece: u8) -> Option<&graphics::Image> {
-        //use chess_logic::*;
-        let mut image: Option<&graphics::Image> = None;
-        if piece == 0 {
-            image = None;
-        } else if is_black_king(piece) {
-            image = Some(&self.black_king);
-        } else if is_black_queen(piece) {
-            image = Some(&self.black_queen);
-        } else if is_black_rook(piece) {
-            image = Some(&self.black_rook);
-        } else if is_black_bishop(piece) {
-            image = Some(&self.black_bishop);
-        } else if is_black_knight(piece) {
-            image = Some(&self.black_knight);
-        } else if is_black_pawn(piece) {
-            image = Some(&self.black_pawn);
-        } else if is_white_king(piece) {
-            image = Some(&self.white_king);
-        } else if is_white_queen(piece) {
-            image = Some(&self.white_queen);
-        } else if is_white_rook(piece) {
-            image = Some(&self.white_rook);
-        } else if is_white_bishop(piece) {
-            image = Some(&self.white_bishop);
-        } else if is_white_knight(piece) {
-            image = Some(&self.white_knight);
-        } else if is_white_pawn(piece) {
-            image = Some(&self.white_pawn);
+    pub fn get_board_piece_image(&mut self, chess_piece: ChessPiece) -> Option<&graphics::Image> {
+        let image: Option<&graphics::Image>;
+        match (chess_piece.color, chess_piece.id) {
+            (ChessPieceColor::Black, ChessPieceId::King) => image = Some(&self.black_king),
+            (ChessPieceColor::Black, ChessPieceId::Queen) => image = Some(&self.black_queen),
+            (ChessPieceColor::Black, ChessPieceId::Rook) => image = Some(&self.black_rook),
+            (ChessPieceColor::Black, ChessPieceId::Bishop) => image = Some(&self.black_bishop),
+            (ChessPieceColor::Black, ChessPieceId::Knight) => image = Some(&self.black_knight),
+            (ChessPieceColor::Black, ChessPieceId::Pawn) => image = Some(&self.black_pawn),
+            (ChessPieceColor::White, ChessPieceId::King) => image = Some(&self.white_king),
+            (ChessPieceColor::White, ChessPieceId::Queen) => image = Some(&self.white_queen),
+            (ChessPieceColor::White, ChessPieceId::Rook) => image = Some(&self.white_rook),
+            (ChessPieceColor::White, ChessPieceId::Bishop) => image = Some(&self.white_bishop),
+            (ChessPieceColor::White, ChessPieceId::Knight) => image = Some(&self.white_knight),
+            (ChessPieceColor::White, ChessPieceId::Pawn) => image = Some(&self.white_pawn)
         }
         return image;
-    }*/
+    }
 
-    pub fn draw_chess_board(&mut self, board: [u8; 64], ctx: &mut Context) -> GameResult<()> {
-        let mut rank  = 0;
+    pub fn draw_chess_board(&mut self, ctx: &mut Context) -> GameResult<()> {
         let mut grabbed_piece_pos: Option<ggez::mint::Point2<u8>> = None;
-        let mut grabbed_piece: Option<u8> = None;
+        let mut grabbed_piece: Option<ChessPiece> = None;
         if ggez::input::mouse::button_pressed(ctx, ggez::input::mouse::MouseButton::Left) 
         || ggez::input::mouse::button_pressed(ctx, ggez::input::mouse::MouseButton::Right) {
             let mouse_down_board_pos = get_square_from_mouse_pos(self.mouse_button_press_down.unwrap());
@@ -140,55 +127,61 @@ impl MyGame {
             }
         }
 
-        for piece in board.iter() {
-            let piece = *piece;
-            let x = rank % 8;
-            let y = rank / 8;
-
-            let image = &self.black_square;
-            let scale_factor = (SCREEN_WIDTH) / (image.dimensions().h*8.0);
-            let scale = glam::Vec2::new(scale_factor, scale_factor);
-            let size = image.dimensions().h * scale.x;
-            let dst = glam::Vec2::new(size*x as f32, size*y as f32);
-
-            if (x + y) % 2 == 0 {
-                graphics::draw(ctx, &self.black_square, graphics::DrawParam::new()
-                .dest(dst)
-                .scale(scale),)?;
-            }
-            else {
-                graphics::draw(ctx, &self.white_square, graphics::DrawParam::new()
-                .dest(dst)
-                .scale(scale),)?;
-            }
-
-            if grabbed_piece_pos.is_some() 
-                && grabbed_piece_pos.unwrap().x == x 
-                && grabbed_piece_pos.unwrap().y == y {
-                grabbed_piece = Some(piece);
-                // Draw the piece later ontop of other pieces
-            }
-            else  {
-                //let image: Option<&graphics::Image> = self.get_board_piece_image(piece);
-                let image = Some(&self.black_rook);
-                if image.is_some() {
-                    let image = image.unwrap();
+        for x in 0..8 {
+            for y in 0..8 {
+                // Draw black and white squares
+                {
+                    let image = &self.black_square;
                     let scale_factor = (SCREEN_WIDTH) / (image.dimensions().h*8.0);
                     let scale = glam::Vec2::new(scale_factor, scale_factor);
                     let size = image.dimensions().h * scale.x;
                     let dst = glam::Vec2::new(size*x as f32, size*y as f32);
-                    graphics::draw(ctx, image, graphics::DrawParam::new()
+
+                    if (x + y) % 2 == 0 {
+                        graphics::draw(ctx, &self.black_square, graphics::DrawParam::new()
                         .dest(dst)
                         .scale(scale),)?;
+                    }
+                    else {
+                        graphics::draw(ctx, &self.white_square, graphics::DrawParam::new()
+                        .dest(dst)
+                        .scale(scale),)?;
+                    }
+                }
+
+                // Draw the chess piece
+                let chess_piece = self.game.get_board_piece_clone(BoardPosition::new(x, y));
+                if chess_piece.is_some() {
+                    let chess_piece = chess_piece.unwrap();
+
+                    if grabbed_piece_pos.is_some() 
+                        && grabbed_piece_pos.unwrap().x == x 
+                        && grabbed_piece_pos.unwrap().y == y {
+                        grabbed_piece = Some(chess_piece);
+                        // Draw the piece later ontop of other pieces
+                    }
+                    else  {
+                        let image: Option<&graphics::Image> = self.get_board_piece_image(chess_piece);
+                        //let image = Some(&self.black_rook);
+                        if image.is_some() {
+                            let image = image.unwrap();
+                            let scale_factor = (SCREEN_WIDTH) / (image.dimensions().h*8.0);
+                            let scale = glam::Vec2::new(scale_factor, scale_factor);
+                            let size = image.dimensions().h * scale.x;
+                            let dst = glam::Vec2::new(size*x as f32, size*y as f32);
+                            graphics::draw(ctx, image, graphics::DrawParam::new()
+                                .dest(dst)
+                                .scale(scale),)?;
+                        }
+                    }
                 }
             }
+        }
 
-            rank += 1;
-        } 
         // Draw if player has grabbed a piece
         if grabbed_piece_pos.is_some() && grabbed_piece.is_some() {
-            let image = Some(&self.black_rook);
-            //let image = self.get_board_piece_image(grabbed_piece.unwrap());
+            //let image = Some(&self.black_rook);
+            let image = self.get_board_piece_image(grabbed_piece.unwrap());
             if image.is_some() {
                 let image = image.unwrap();
                 let scale_factor = (SCREEN_WIDTH) / (image.dimensions().h*8.0);
@@ -255,15 +248,17 @@ impl EventHandler<ggez::GameError> for MyGame {
 
         let mouse_up_board_pos = mouse_up_board_pos.unwrap();
         let mouse_down_board_pos = mouse_down_board_pos.unwrap();
-        let pos_array = ['a', 'b','c','d','e','f','g','h',];
-        let move_notation_mouse_up = pos_array[(mouse_up_board_pos.x) as usize].to_string() + (8-mouse_up_board_pos.y).to_string().as_str();
-        let move_notation_mouse_down = pos_array[(mouse_down_board_pos.x) as usize].to_string() + (8-mouse_down_board_pos.y).to_string().as_str();
 
-        /*let is_valid = chess_logic::move_piece_from_to(move_notation_mouse_down.as_str(), move_notation_mouse_up.as_str(), &mut self.game);
-        println!("{}, {}", move_notation_mouse_down, move_notation_mouse_up);
-        if !is_valid {
+        let board_move = BoardMove::new(
+            mouse_down_board_pos.x, 
+            mouse_down_board_pos.y, 
+            mouse_up_board_pos.x, 
+            mouse_up_board_pos.y);
+
+        let move_result = self.game.move_piece(board_move, true, Some(ChessPieceId::Queen));
+        if move_result.is_err() {
             println!("{}", "not a valid move");
-        }*/
+        }
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
@@ -271,7 +266,7 @@ impl EventHandler<ggez::GameError> for MyGame {
         // Draw code here...
 
         //let board = self.game.get_board();
-        //self.draw_chess_board(board, ctx)?;
+        self.draw_chess_board(ctx)?;
         graphics::present(ctx)
     }
 }
